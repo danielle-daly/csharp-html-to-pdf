53b2d8d639114964b38f686badee5afe1d4355ce
Danielle <danielledaly9@gmail.com>
2024-12-10 11:06:04 +0000

    init commit

---
+++ RandomTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using iText.Html2pdf;
+
+namespace RandomTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // TODO: set path to files 
+            var path = "C:\\";
+            var inputFile = new FileInfo(path + @"\test.html");
+            var outputFile = new FileInfo(path + @"\test.pdf");
+            HtmlConverter.ConvertToPdf(inputFile, outputFile);
+
+        }
+    }
+}
